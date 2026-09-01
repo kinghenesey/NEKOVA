00c75521b79b87dedaf356908b61cb67f7313efd
unknown <emmanuelkingchris@gmail.com>
2026-09-01 10:17:14 +0100

    updated the grammer coverage check file

--- tools/check_grammar_coverage.py
+++ tools/check_grammar_coverage.py
@@ -63,6 +63,7 @@ PARSE_METHOD_TO_GRAMMAR_RULES = {
     "_parse_imagine":              ["imagine_stmt"],
     "_parse_imagine_expr":         ["imagine_expr"],
     "_parse_shape":                ["shape_def"],
+    "_parse_schema_def":           ["schema_def"],
     "_parse_watch":                ["watch_stmt"],
     "_parse_yield":                ["yield_stmt"],
     "_parse_decorator":            ["decorator_stmt"],
